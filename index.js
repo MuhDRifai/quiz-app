778ac64751d23fe3a55f0b2591d329d14f3025d2
MuhDRifai <muh.d.rifai23@gmail.com>
2022-07-06 07:48:28 +0700

    new commit

---
+++ index.js
@@ -0,0 +1,389 @@
+const questions = [
+    {
+        question: "Kapankah BPUPKI didirikan?",
+                optionA: "29 April 1945",
+                optionB: "29 Mei 1945",
+                optionC: "1 Juni 1945",
+                optionD: "7 Agustus 1945",
+                correctOption: "optionA"
+    },
+
+    {
+        question: "siapakah pendiri BPUPKI?",
+        optionA: "IR. Soekarno",
+        optionB: "Moh. Yamin",
+        optionC: "Kumakichi Harada",
+        optionD: "Hisaichi Terauchi",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Masyarakat Hindu melihat waktu sebagai?",
+                optionA: "akhir",
+                optionB: "garis lurus",
+                optionC: "siklus tanpa akhir",
+                optionD: "awal",
+                correctOption: "optionC"
+    },
+
+    {
+        question: "Pada tahun 1957-1975 terjadi Perang Vietnam yang merupakan bagian dari perang dingin antara kubu ideologi besar Komunis dan Liberal yang dikenal dengan perang?",
+        optionA: "Indocina Pertama ",
+        optionB: "Indocina Kedua",
+        optionC: "Perang Vietnam",
+        optionD: "Perang Komunis",
+        correctOption: "optionB"
+    },
+
+    {
+        question: " Pada masa pemerintahan raja balaputradewa, diketahui bahwa pusat pusat kedudukan kerajaansyailendra terletak di daerah pegunungan di sebealh selatan. Hal ini berdasarkan pada letak peninggalan?",
+                optionA: "Istana ratu boko",
+                optionB: " Istana sekar taji",
+                optionC: "Istana balaputradewa",
+                optionD: "Semua salah",
+                correctOption: "optionA"
+    },
+
+    {
+        question: "candi kalasan, merupakan peninggalan dinasti syailendra. Candi itu merupakan candi tertua di pulau jawa yang dibangun sekitar tahun?",
+        optionA: "700 M",
+        optionB: "500 M",
+        optionC: "800 M",
+        optionD: "400 M",
+        correctOption: "optionC"
+    },
+
+    {
+        question: " Kerajaan mataram kuno terletak di jawa tengah dengan daerah pusatyang dikelilingi oleh pegunungan dan gunung-gunung yang disebut?",
+        optionA: "Mataram kuno",
+        optionB: " Jaya mataram",
+        optionC: "Bhumi mataram",
+        optionD: " Mataram jawa",
+        correctOption: "optionC"
+    },
+
+    {
+        question: " Raja jayanegara merupakan putra dari raja wijaya.sebelum raja jayanegara nak tahta, beliau bernama..?",
+                optionA: "Kala gemot",
+                optionB: "Saputrariarawan",
+                optionC: " Latusindron",
+                optionD: "kurupati",
+                correctOption: "optionA"
+    },
+
+    {
+        question: "tribhuwanatunggadewi memerintah kerajaan majapahit dari tahun 1328-1350. Pada masa pemerintahannya, terjadi pemberontakan sadeng, yang berlangsung pada tahun...?",
+        optionA: "1234",
+        optionB: "1456",
+        optionC: "1221",
+        optionD: "1331",
+        correctOption: "optionD"
+    },
+
+    {
+        question: " raja jayanegara meninggal dengan tidak meninggalkan seorang pewaris tahta. Tahta kerajaan majapahitpun jatuh ketangan gayatri. Siapakah gayatri??",
+                optionA: "Istri jayanegara",
+                optionB: "Anak nahendranudita",
+                optionC: "Anak prajnaparamita",
+                optionD: "Putri raja kertanegara",
+                correctOption: "optionD"
+    },
+
+    {
+        question: "Hayam wuruk merupakan raja dengan pandangan luas.beliau memerinahkerajaan majapahit sejak tahun.....sampai....?",
+        optionA: "1350-1389",
+        optionB: "1362-1496",
+        optionC: "1222-1333",
+        optionD: "1459-1470",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Raja hayamwuruk merupakan salah satu raja yang memerintah kerajaan majapahit. Raja hayam wuruk terlahir dari pernikahan cakraddara dan?",
+        optionA: "Tribhuwaratunggadewi",
+        optionB: " Mahadewi",
+        optionC: "Jayendradewi",
+        optionD: "Gayatri",
+        correctOption: "optionA"
+    },
+
+
+    {
+        question: "Sumber informasi mengenai berdiri dan berkembangnya kerajaan majapahit berasal  dari beberapa sumber, kitab yang menceritakan tentang raja-raja singasari dan majapahit adalah?",
+                optionA: "Kitab negarakertagama",
+                optionB: "Prasasti butak",
+                optionC: "Kidung harsawijaya dan kidung panji wijayakama",
+                optionD: "Kitab parahraton",
+                correctOption: "optionD"
+    },
+
+    {
+        question: " Naskah proklamasi yang autentik adalah?",
+        optionA: "ditulis tangan oleh Soekarno",
+        optionB: "diketik oleh Sayuti Melik",
+        optionC: "dibuat oleh Soekarno",
+        optionD: "ditulis oleh Moh. Yamin",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Kota Nagasaki dijatuhi bom atom oleh Sekutu pada?",
+                optionA: "29 April 1945",
+                optionB: "29 Mei 1945",
+                optionC: "1 Juni 1945",
+                optionD: "9 Agustus 1945",
+                correctOption: "optionD"
+    },
+
+    {
+        question: "Berita menyerahnya Jepang dapat diketahui oleh seorang tokoh pemuda melalui?",
+        optionA: "pengumuman di koran-koran",
+        optionB: "surat-surat kabar",
+        optionC: "radio BBC",
+        optionD: "selebaran dari Sekutu",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Ketika tiba di Jakarta dari RengasdengklokSoekarno, Moh. Hatta, dan Ahmad Subardjo segera menemui?",
+        optionA: "Kumakichi Harada",
+        optionB: "Jenderal Nishimura",
+        optionC: "Hisaichi Terauchi",
+        optionD: "Laksamana Maeda",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "KNIP dibentuk pada tanggal?",
+                optionA: "29 April 1945",
+                optionB: "29 Mei 1945",
+                optionC: "18 Agustus 1945",
+                optionD: "19 Agustus 1945",
+                correctOption: "optionC"
+    },
+
+    {
+        question: "Presiden Soekarno secara resmi mengumumkan pembentukan BKR pada?",
+        optionA: "23 Agustus 1945",
+        optionB: "19 Agustus 1945",
+        optionC: "31 Juni 1945",
+        optionD: "18 Agustus 1946",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Salah satu upaya pemerintah Indonesia untuk mengisi kembali perbendaharaan pada awal kemerdekaan adalah?",
+                optionA: " menasionalisasi De Javasche Bank",
+                optionB: "Devaluasi rupiah",
+                optionC: "sistem ekonomi pergerakan benteng",
+                optionD: "memegang pinjaman nasional",
+                correctOption: "optionD"
+    },
+
+    {
+        question: "Salah satu dampak dari Perang Dunia I yang terjadi sejak 1914 hingga 1918 adalah hancurnya 4 kekaisaran di Eropa yang sudah ada sejak abad ke 17, keempat kekaisaran yang hancur tersebut antara lain adalah?",
+        optionA: "Serbia, Montenegro, Jerman, Austria-Hongaria",
+        optionB: "Jerman, Inggris, Rusia, Perancis",
+        optionC: " Austria-Hongaria, Jerman, Rusia, Turki Usmani",
+        optionD: "Rusia, Serbia, Bosnia, Ukraina",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Perang Dunia I melibatkan dua aliansi yang bersaing, salah satu aliansi tersebut adalah Triple entente yang terdiri dari negara?",
+                optionA: "Inggris, Amerika Serikat dan Italia",
+                optionB: "Perancis, Inggris, Rusia",
+                optionC: "Rusia, Amerika Serikat, Serbia",
+                optionD: "Austria-Hongaria, Serbia, Rusia",
+                correctOption: "optionB"
+    },
+
+    {
+        question: "Siapakah yang mendapat gelar De Ongekroonde van java?",
+        optionA: "R.M. Tjokroamiseno",
+        optionB: "R.M. Adipati Tjokronegoro",
+        optionC: "H. Samanhudi",
+        optionD: "H.O.S Tjokroaminoto",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "siapakah delegasi Indonesia dalam KMB?",
+        optionA: "Ir. Soekarno",
+        optionB: "Moh. Yamin",
+        optionC: "Sultan PB.IX",
+        optionD: "Drs.Moh.Hatta",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Dimanakah Koferensi Meja Bundar diadakan?",
+        optionA: "Amerika Serikat",
+        optionB: "Uni Soviet",
+        optionC: "Jerman",
+        optionD: "Belanda",
+        correctOption: "optionD"
+    }
+
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
+
+function handleQuestions() { 
+    //function to shuffle and push 10 questions to shuffledQuestions array
+//app would be dealing with 10questions per session
+    while (shuffledQuestions.length <= 9) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1 //holds the current question number
+let playerScore = 0  //holds the player score
+let wrongAttempt = 0 //amount of wrong answers picked by player
+let indexNumber = 0 //will be used in displaying next question
+
+// function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++ //adding to player's score
+            indexNumber++ //adding 1 to index so has to display next question..
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++ //adds 1 to wrong attempts 
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer() //check if player picked right or wrong option
+    unCheckRadioButtons()
+    //delays next question displaying for a second just for some effects so questions don't rush in on player
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 3) {
+        remark = "Bad Grades, Keep Practicing."
+        remarkColor = "red"
+    }
+    else if (playerScore >= 4 && playerScore < 7) {
+        remark = "Average Grades, You can do better."
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 7) {
+        remark = "Excellent, Keep the good work going."
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 10) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal, resets game and reshuffles questions
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
